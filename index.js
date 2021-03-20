c29d18279c199420783f3acf1035b3802980a1a8
burtk33 <burtk33@gmail.com>
2021-03-19 21:43:49 -0400

    adding view all employees function

--- index.js
+++ index.js
@@ -32,37 +32,30 @@ const menu = () => {
         switch (menuSelect) {
             case 'View all employees': {
                 displayAllEmployees();
-                menu();
                 break;
             }
             case 'View all employees by department': {
                 displayAllbByDept();
-                menu();
                 break;
             }
             case 'View all employees by manager': {
                 displayAllByMgnr();
-                menu();
                 break;
             }
             case 'Add employee': {
                 addEmployee();
-                menu();
                 break;
             }
             case 'Remove Employee': {
                 removeEmployee();
-                menu();
                 break;
             }
             case 'Update employee role': {
                 updateRole();
-                menu();
                 break;
             }
             case 'Update employee manager': {
                 updateMgnr();
-                menu();
                 break;
             }
             case 'Exit': {
@@ -77,8 +70,11 @@ const menu = () => {
 
 //display to console all employees in database
 const displayAllEmployees = () => {
-    connection.query('SELECT * FROM employee', (err, res) => {
-        if (err) throw (err);
-        console.table("\nAll employees",res);
-    });
+    connection.query("SELECT employee.first_name AS 'First Name', employee.last_name AS 'Last Name', role.title AS 'Title', role.salary AS 'Salary', department.name AS 'Department', CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;",
+        function (err, res) {
+            if (err) throw err
+            console.table(res)
+            menu()
+        }
+    )
 }
